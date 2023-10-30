f7853b60ac2f79af8670f11a0726e03d16be39ba
Jasper <jasper.precht@web.de>
2023-10-30 11:40:01 +0100

    update locales

--- test.ts
+++ test.ts
@@ -2,6 +2,6 @@
 SI1151.initSunlight()
 basic.forever(function () {
     serial.writeLine("Licht:" + SI1151.getHalfWord_Visible())
-    serial.writeLine("IR: " + SI1151.getHalfWordIR())
-    serial.writeLine("UV: " + SI1151.getHalfWordUV())
+    serial.writeLine("IR: " + SI1151.getHalfWord_IR())
+    serial.writeLine("UV: " + SI1151.getHalfWord_UV())
 })
